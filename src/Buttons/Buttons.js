0e52edb86726b251a7c4bf8a3d6918f6bf3ddffd
DinosMpo <dinos7591@yahoo.gr>
2025-02-03 21:38:31 +0200

    code for the break functionality

--- src/Buttons/Buttons.js
+++ src/Buttons/Buttons.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Buttons.css';
 
-export default function Buttons({resetTimer, start, setStart}) {
+export default function Buttons({resetTimer, start, setStart, counterTime, breakActive}) {
     return (
         <div id="buttons-container">
-            <div id="start_stop" onClick={() => setStart(prev => !prev)}>{!start ? 'start' : 'stop'}</div>
+            <div id="start_stop" onClick={() => {
+                setStart(prev => !prev);
+                // if(true) {}
+            }}>{!start ? 'start' : 'stop'}</div>
             <div id="reset" onClick={() => resetTimer()}>reset</div>
         </div>
     )
--- src/Time/Time.js
+++ src/Time/Time.js
@@ -1,21 +1,31 @@
 import { useEffect } from 'react'
 import './Time.css';
 
-export default function Time({counterTime ,setCounterTime, time, start, setStart, sessionTime, setTime }) {
+export default function Time({ counterTime, setCounterTime, time, start, setStart, sessionTime, setTime, breakTime, breakActive, setBreakActive }) {
     //time 25:00
     // const [counterTime, setCounterTime] = useState(Number((time[0] + time[1]) * 60 * 1000) + (Number(time[3] + time[4]) * 1000));
-    
+
     //if the timer have start and every second run this code
     useEffect(() => {
-        if(start) {
+        if (start) {
+            // if(counterTime == 0 && breakActive == false) {
+            //     setCounterTime(Number((time[0] + time[1]) * 60 * 1000) + (Number(time[3] + time[4]) * 1000));
+            // }
             const interval = setInterval(() => {
-                if(counterTime > 0) {
+                if (counterTime > 0) {
                     setCounterTime(counterTime - 1000);
                     // setTime(time - 1);
-                }else{
-                    setStart(false);
+                } else if (counterTime == 0 && breakActive == false) {
+                    // setStart(false);
+                    setBreakActive(true);
+                    setCounterTime(breakTime * 60 * 1000);
+                } else if (counterTime == 0 && breakActive == true) {
+                    // setStart(false);
+                    setBreakActive(false);
+                    setCounterTime(time.length > 4 ? Number((time[0] + time[1]) * 60 * 1000) + (Number(time[3] + time[4]) * 1000) : Number((time[0]) * 60 * 1000) + (Number(time[2] + time[3]) * 1000));
                 }
-                console.log(counterTime);
+                console.log('counterTime ', counterTime);
+                console.log('time ', time);
             }, 1000);
             return () => clearInterval(interval);
         }
@@ -38,7 +48,7 @@ export default function Time({counterTime ,setCounterTime, time, start, setStart
 
     return (
         <div id="time-container">
-            <div id="timer-label">Session</div>
+            <div id="timer-label" className={breakActive ? 'break-time' : ''}>{breakActive ? 'Break' : 'Session'}</div>
             <div id="time-left">{formatTime()}</div>
         </div>
     )
